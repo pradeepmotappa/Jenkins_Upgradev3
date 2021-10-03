c01de1254e48145cfa6608ac40dda39ac8dc7d5e
Pradeep M <pradeepm1002@gmail.com>
2021-10-03 18:08:22 +0530

    Added name to Pradeep and Poll SCM Error

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
   }
 
   
-  public static void main(String args[]){
+  publics static void main(String args[]){
     
 	  System.out.println("Hello! My Name is Pradeep!!! This is a Poll SCM Example");
   }
