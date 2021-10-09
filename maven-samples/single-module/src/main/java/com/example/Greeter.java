b5c6254a8216b8a75462145e9f8327f48ce64a41
Pradeep M <pradeepm1002@gmail.com>
2021-10-09 17:20:55 +0530

    Archieve Test No 1

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,6 +21,6 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("I Tested this Code and Its working fine.");
+	  System.out.println("Archieve Test.");
   }
 }
