4f765d10bfbf12d7cba2c9a10599ffe3e12fdcdd
pradeepmotappa <44636917+pradeepmotappa@users.noreply.github.com>
2021-12-11 19:39:59 +0530

    Added New Line

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,6 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("SUCCESS!!! Maven & JAR Executing Currectly!");
+	  System.out.println("SUCCESS AGAIN!!! Maven & JAR Executing Currectly!");
+	  System.out.println("This Execution Successfully completed using Jenkins Pipeline");
   }
 }
